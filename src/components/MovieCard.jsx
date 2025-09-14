a2713dc9196167b85702edea93bbbd3b5b7fd1fa
Osaid <osaidzagha7000@gmail.com>
2025-09-14 12:29:31 +0300

    Set up React Router and ShowPage navigation

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,14 +1,18 @@
+import { Link } from "react-router-dom";
+
 export default function MovieCard({ movie }) {
   if (!movie.poster_path) return null;
 
   return (
-    <div className="cursor-pointer hover:scale-105 transform transition">
-      <img
-        src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-        alt={movie.title}
-        className="w-full h-64 object-cover rounded-lg"
-      />
-      <h3 className="mt-2 text-sm font-semibold">{movie.title}</h3>
-    </div>
+    <Link to={`/movie/${movie.id}`}>
+      <div className="cursor-pointer hover:scale-105 transform transition">
+        <img
+          src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+          alt={movie.title}
+          className="w-full h-64 object-cover rounded-lg"
+        />
+        <h3 className="mt-2 text-sm font-semibold">{movie.title}</h3>
+      </div>
+    </Link>
   );
 }
